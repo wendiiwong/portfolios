679ec51af16a65ad7bbefeb4cf8d1754a38d2d7c
wendiiwong <birdiibuddy007@gmail.com>
2020-01-20 11:11:56 +0100

    Updates

---
+++ precache-manifest.789b857283e3247fa5e627ed98d9e27f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560e2ee5a2aeeaf13fd3bbcdad6bb43a",
+    "url": "/portfolios/index.html"
+  },
+  {
+    "revision": "5ee55843bec7cee147ad",
+    "url": "/portfolios/static/css/main.5b5f1e6c.chunk.css"
+  },
+  {
+    "revision": "1179fa5e50a8ee7010f1",
+    "url": "/portfolios/static/js/2.81d69104.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolios/static/js/2.81d69104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ee55843bec7cee147ad",
+    "url": "/portfolios/static/js/main.1d6b1fd5.chunk.js"
+  },
+  {
+    "revision": "f1b7039c18497cc74771",
+    "url": "/portfolios/static/js/runtime-main.5098ee8d.js"
+  }
+]);
